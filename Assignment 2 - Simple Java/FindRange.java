09f6d7b254104f5f3a227e7093c634e578891fc6
Bryan Payne <32659397+brygit24@users.noreply.github.com>
2021-10-13 16:17:31 -0700

    Update FindRange.java

--- Assignment 2 - Simple Java/FindRange.java
+++ Assignment 2 - Simple Java/FindRange.java
@@ -13,52 +13,50 @@ public class FindRange extends ConsoleProgram {
 	public static void main(String [] args) {
 	    new FindRange().start(args);
 	}
-	
 	public void run () {		
-		int intCount = 0;
-		int smallest = 0;
-		int largest = 0;
+	    int intCount = 0;
+	    int smallest = 0;
+	    int largest = 0;
 		
-		println("This program finds the largest and smallest numbers");
-		println("When you are done entering integers, enter 0 to exit.");
+	    println("This program finds the largest and smallest numbers");
+	    println("When you are done entering integers, enter 0 to exit.");		
 		
-		
-		while (true) {			
-			int n1 = readInt("? ");			
-			if (n1 == 0 && intCount == 0) {				
-				break;
-			}				
-			if (n1 == 0 && intCount == 1) {				
-				break;
-			}
-			if (n1 == 0) {				
-				break;			
-			}			
-			else {
-				intCount++;				
-				if (intCount == 1) {
-					smallest = n1;
-					largest = n1;
-				}
-				if (n1 < smallest) {
-					smallest = n1;
-				}
-				if (n1 > largest) {
-					largest = n1;
-				}				
-			}			
+	    while (true) {			
+	        int n1 = readInt("? ");			
+		if (n1 == 0 && intCount == 0) {				
+		    break;
+		}				
+		if (n1 == 0 && intCount == 1) {				
+		    break;
 		}
-		if (intCount == 0) {
-			println("You did NOT enter any values!!");
+		if (n1 == 0) {				
+		    break;			
 		}
-		if (intCount == 1) {
-			println("one entry " + largest + " Are both, lowest and smallest " + smallest);
-		}
-		else if (intCount > 1) {
-			println("Smallest: " + smallest);
-			println("Largest: " + largest);	
-		}					
-	}			
+		else {
+		    intCount++;				
+		    if (intCount == 1) {
+		        smallest = n1;
+			largest = n1;
+		    }
+		    if (n1 < smallest) {
+		        smallest = n1;
+		    }
+		    if (n1 > largest) {
+		        largest = n1;
+		    }				
+		}			
+	    }
+	    if (intCount == 0) {
+	        println("You did NOT enter any values!!");
+	    }
+	    if (intCount == 1) {
+	        println("one entry " + largest + " Are both, lowest and smallest " + smallest);
+	    }
+	    else if (intCount > 1) {
+	        println("Smallest: " + smallest);
+		println("Largest: " + largest);	
+	    }					
+	}     
 }
 		
 
