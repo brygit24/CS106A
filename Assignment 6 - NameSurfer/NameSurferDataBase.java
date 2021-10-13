9be27341a46b0065ea9f061eb0db82f77911ca62
Bryan Payne <32659397+brygit24@users.noreply.github.com>
2021-10-13 14:37:37 -0700

    Update NameSurferDataBase.java

--- Assignment 6 - NameSurfer/NameSurferDataBase.java
+++ Assignment 6 - NameSurfer/NameSurferDataBase.java
@@ -25,31 +25,31 @@ public class NameSurferDataBase implements NameSurferConstants {
  * 
  */
 	public NameSurferDataBase(String fileName) {		
-		loadDataBase(fileName);						
+	    loadDataBase(fileName);						
 	}
 	// Loading the database and making accessible to this 
 	// program via MAPPING into namesHashMap, which tracks
 	// names ALREADY displayed in the graph.	
 	private void loadDataBase(String inTextFile) {
-		try {
-			BufferedReader bReader = new BufferedReader(new FileReader(inTextFile));
-			while (true) {
-				String bLine = bReader.readLine();
-				if (bLine == null) {
-					break;
-				}
-				else {
-					// must go into the hashmap in the form of the "NameSurferEntry" data type
-					NameSurferEntry bEntry = new NameSurferEntry(bLine);					
-					String key = bEntry.getName();
-					namesHashMap.put(key,bEntry);
-				}   
-			}
-			bReader.close();
+	    try {
+	        BufferedReader bReader = new BufferedReader(new FileReader(inTextFile));
+		while (true) {
+		    String bLine = bReader.readLine();
+		    if (bLine == null) {
+		        break;
+		    }
+		    else {
+		        // must go into the hashmap in the form of the "NameSurferEntry" data type
+			NameSurferEntry bEntry = new NameSurferEntry(bLine);					
+			String key = bEntry.getName();
+			namesHashMap.put(key,bEntry);
+		    }   
 		}
-		catch(IOException ex) {
-			throw new ErrorException(ex);
-		}	
+		bReader.close();
+	      }
+	      catch(IOException ex) {
+	      throw new ErrorException(ex);
+	}	
 	}	
 	/* FIND ENTRY IN DATABASE OF NAMES DISPLAYED ON THE GRAPH!!
 	*  Returns the nameSurferEntry associated with the name, if one
@@ -57,7 +57,7 @@ public class NameSurferDataBase implements NameSurferConstants {
 	*  method returns null. 
 	*/
 	public NameSurferEntry findEntry(String name) {		
-		// perform a get on the names hash map
+	    // perform a get on the names hash map
 	    return namesHashMap.get(name.toLowerCase());		
 	}
 	// hashmap of NameSurferEntry data types.
